f2e45682c728ac62b8fc39ad405df6a8d10a6617
Buddalaz <sandunsampath289@gmail.com>
2023-06-11 15:34:20 +0530

    my first changes refactor add login register

--- php/registration.php
+++ php/registration.php
@@ -1,107 +1,46 @@
 <?php
-// Database connection details
-$hostname = "localhost";
-$username = "root";
-$password = "";
-$database = "bus";
 
-// Create a connection to the MySQL database
-$conn = new mysqli($hostname, $username, $password, $database);
+    require "../database/connection.php";
 
-// Check if the connection was successful
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-}
+    // Check if the registration form was submitted
+    if ($_SERVER["REQUEST_METHOD"] == "POST") {
+        // Get the username and password from the form
+        $username = $_POST["username"];
+        $password = $_POST["password"];
+        $lastName = $_POST["lastName"];
+        $firstName = $_POST["firstName"];
+        $address = $_POST["address"];
+        $contactNo = $_POST["contact"];
 
-// Check if the registration form was submitted
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    // Get the username and password from the form
-    $username = $_POST["username"];
-    $password = $_POST["password"];
+        // Prepare and execute the SQL statement to insert the user data into the database
+        $stmt = $conn->prepare("INSERT INTO users (username, password, lastName, firstName, address, contact) VALUES (?, ?, ?, ?, ?, ?)");
+        $stmt->bind_param("ssssss", $username, $password, $lastName, $firstName, $address, $contactNo);
+        $stmt->execute();
 
-    // Prepare and execute the SQL statement to insert the user data into the database
-    $stmt = $conn->prepare("INSERT INTO users (username, password) VALUES (?, ?)");
-    $stmt->bind_param("ss", $username, $password);
-    $stmt->execute();
+        // Check if the user was successfully registered
+        if ($stmt->affected_rows === 1) {
+            $message = "User registered successfully.";
+            header("Location: welcome.php");
+        } else {
+            $message = "Error registering user.";
+        }
 
-    // Check if the user was successfully registered
-    if ($stmt->affected_rows === 1) {
-        $message = "User registered successfully.";
-    } else {
-        $message = "Error registering user.";
+        // Close the prepared statement
+        $stmt->close();
     }
 
-    // Close the prepared statement
-    $stmt->close();
-}
-
-// Close the database connection
-$conn->close();
+    // Close the database connection
+    $conn->close();
 ?>
 
 <!DOCTYPE html>
 <html>
 <head>
     <title>User Registration</title>
-    <style>
-        body {
-            font-family: Arial, sans-serif;
-        }
-
-        .container {
-            width: 400px;
-            margin: 0 auto;
-            padding: 20px;
-            background-color: #f4f4f4;
-            border-radius: 5px;
-            box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
-        }
-
-        h2 {
-            text-align: center;
-        }
-
-        label {
-            display: block;
-            margin-bottom: 5px;
-        }
-
-        input[type="text"],
-        input[type="password"] {
-            width: 100%;
-            padding: 10px;
-            border: 1px solid #ccc;
-            border-radius: 3px;
-            margin-bottom: 10px;
-        }
-
-        input[type="submit"] {
-            width: 100%;
-            padding: 10px;
-            background-color: #4CAF50;
-            border: none;
-            color: #fff;
-            cursor: pointer;
-            border-radius: 3px;
-        }
-
-        input[type="submit"]:hover {
-            background-color: #45a049;
-        }
-
-        .success-message {
-            color: green;
-            text-align: center;
-        }
-
-        .error-message {
-            color: red;
-            text-align: center;
-        }
-    </style>
+    <link rel="stylesheet" href="../css/styles.css">
 </head>
 <body>
-    <div class="container">
+    <div class="regcontainer">
         <?php if (isset($message)): ?>
             <div class="success-message"><?php echo $message; ?></div>
         <?php endif; ?>
@@ -111,8 +50,17 @@ $conn->close();
             <input type="text" id="username" name="username" required><br><br>
             <label for="password">Password:</label>
             <input type="password" id="password" name="password" required><br><br>
+            <label for="firstName">First Name:</label>
+            <input type="text" id="firstName" name="firstName" required><br><br>
+            <label for="lastName">Last Name:</label>
+            <input type="text" id="lastName" name="lastName" required><br><br>
+            <label for="address">Address:</label>
+            <input type="text" id="address" name="address" required><br><br>
+            <label for="contact">Contact:</label>
+            <input type="text" id="contact" name="contact" required><br><br>
             <input type="submit" value="Register">
         </form>
+        <!-- <a href="/index.html"><button>Back</button></a> -->
     </div>
 </body>
 </html>
